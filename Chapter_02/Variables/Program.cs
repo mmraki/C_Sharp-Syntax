4006c565788a2c623e8b63b5b9876c09c8a28cfa
Meraki <convallariameraki@gmail.com>
2026-08-22 17:41:08 +0800

    Variables

---
+++ Chapter_02/Variables/Program.cs
@@ -0,0 +1,34 @@
+﻿//Variables
+Console.WriteLine("-----Variables-----");
+int x;    //Declaration
+x = 100;  //Initialization
+
+int y = 200; // Declaration + Initialization
+
+int z = x + y;
+//You can do this
+
+Console.WriteLine("Int x = " + x);
+Console.WriteLine("Int y = " + y);
+Console.WriteLine("The sum of x + y is: " + z);
+
+int num = 1;
+double dbl = 2.5;
+bool state = true;
+char sym = '#';
+String name = "Ram";
+String lastName = "Lazaro";
+String fullName = name + " " + lastName;
+
+Console.WriteLine("Your Number is: " + num);
+Console.WriteLine("Your Double is: " + dbl);
+Console.WriteLine("The State is: " + state);
+Console.WriteLine("Your Symbol is: " + sym);
+Console.WriteLine("Your First Name is: " + name);
+Console.WriteLine("Your Last Name is: " + lastName);
+Console.WriteLine("Your Full Name is: " + fullName);
+
+//Constant
+Console.WriteLine("\n-----Constant-----");
+const double pi = 3.14159;
+Console.WriteLine(pi);
