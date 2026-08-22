293b4765a2348cd2c86c4987ec6bb282895161b2
Meraki <convallariameraki@gmail.com>
2026-08-22 17:44:02 +0800

    If

---
+++ Chapter_08/If_Statements/Program.cs
@@ -0,0 +1,10 @@
+﻿bool state = true;
+
+if (state)
+{
+    Console.WriteLine("Only runs code if statement is true");
+}
+else
+{
+    Console.WriteLine("Will only run if the statement is false");
+}
