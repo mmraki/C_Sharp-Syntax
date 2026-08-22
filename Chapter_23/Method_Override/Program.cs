485f9696b89788e1691ac449513bfedb50337691
Meraki <convallariameraki@gmail.com>
2026-08-22 17:54:08 +0800

    Method Override

---
+++ Chapter_23/Method_Override/Program.cs
@@ -0,0 +1,34 @@
+﻿
+//Method Override = Provides a new version of a method inherited from a parent class
+//                  Must be abstract or virtual to be overriden
+
+Dog dog = new Dog();
+Cat cat = new Cat();
+
+dog.Speak();
+cat.Speak();
+
+
+
+
+class Animal
+{
+    public virtual void Speak()
+    {
+        Console.WriteLine("HEEELP");
+    }
+}
+class Dog : Animal
+{
+    public override void Speak()
+    {
+        Console.WriteLine("Woof");
+    }
+}
+class Cat : Animal
+{
+    public override void Speak()
+    {
+        Console.WriteLine("Meow");
+    }
+}
