0801044a88f9d8169e1e387c7ff689cbaa32ceb9
Meraki <convallariameraki@gmail.com>
2026-08-22 17:40:43 +0800

    Basic Output

---
+++ Chapter_01/Output/Program.cs
@@ -0,0 +1,8 @@
+﻿//Types of Displaying Output
+Console.WriteLine("-----Displaying Output-----");
+Console.WriteLine("Hello, World!");
+//WriteLine(): Outputs the program with a newline character
+Console.Write("Fuck, I'm Learning C#");
+//Write(): Outputs the program
+Console.WriteLine("\n");
+//New Line Character
