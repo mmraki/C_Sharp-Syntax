279efe4a506c73e81a61537a1c1df8b7048ef844
Meraki <convallariameraki@gmail.com>
2026-08-22 17:48:24 +0800

    Class

---
+++ Chapter_18/Class/Messages.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Class
+{
+    internal class Messages
+    {
+        public static void Greet()
+        {
+            Console.WriteLine("Hello, everyone!");
+        }
+        public static void Surprise()
+        {
+            Console.WriteLine("Surprise !");
+        }
+        public static void Goodbye()
+        {
+            Console.WriteLine("Byee, Byeee");
+        }
+    }
+}
---
+++ Chapter_18/Class/Program.cs
@@ -0,0 +1,33 @@
+﻿using Class;
+
+
+//Class = A Group of related code
+//        Blueprint for Objects (OOP)
+
+Messages.Greet();
+Messages.Surprise();
+Messages.Goodbye();
+
+Console.WriteLine();
+
+Status.Hot();
+Status.Cold();
+Status.Warm();
+
+class Status()
+{
+    public static void Hot()
+    {
+        Console.WriteLine("It’s hot in the Philippines.");
+    }
+    public static void Cold()
+    {
+        Console.WriteLine("Its never cold in the Philippines.");
+    }
+    public static void Warm(){
+        Console.WriteLine("Sometimes.");
+    }
+}
+
+
+
