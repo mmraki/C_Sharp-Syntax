4506fff880b53ea69633f70d3bed09070e12730c
Meraki <convallariameraki@gmail.com>
2026-08-22 17:57:38 +0800

    Auto Implemented Properties

---
+++ Chapter_30/Auto_Implemented_ Properties/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Xml.Schema;
+
+
+//Auto Implementation = shortcut when no additional logic to the value is needed
+
+Vehicle Car = new Vehicle("Convertible");
+Console.WriteLine(Car.Type);
+class Vehicle
+{
+    
+     //Field
+     //private String type;
+
+     //public string Type
+     //{
+     //    get { return type; }
+     //    set { type = value; }
+     //}
+     
+    public String Type { get; set; }
+   
+
+    //Constructor
+    public Vehicle(String type)
+    {
+        this.Type = type;
+    }
+}
