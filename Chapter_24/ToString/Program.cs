f8d278f4742504e4b382d5b398b730f6bf99af87
Meraki <convallariameraki@gmail.com>
2026-08-22 17:54:38 +0800

    ToString

---
+++ Chapter_24/ToString/Program.cs
@@ -0,0 +1,33 @@
+﻿
+
+
+Animal Dog = new Animal("Browny", 5, "Brown");
+
+Console.WriteLine(Dog.ToString());
+Console.WriteLine(Dog);
+
+
+ class Animal
+{
+    String name;
+    int age;
+    String color;
+    public Animal(String name, int age, String color){
+        this.name = name;
+        this.age = age;
+        this.color = color;
+    }
+
+    public virtual void Speak()
+    {
+        Console.WriteLine("Speak");
+    }
+    public override string ToString()
+    {
+        String message = $"This is {name}, he is {age} years old and he is color {color}";
+        return message;
+
+        //or
+        // return $"This is {name}, he is {age} years old and he is color {color}";
+    }
+}
