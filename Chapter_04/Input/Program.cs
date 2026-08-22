b2853eabfd692ac0107b9542d8dc5e66827d1583
Meraki <convallariameraki@gmail.com>
2026-08-22 17:42:19 +0800

    User Input

---
+++ Chapter_04/Input/Program.cs
@@ -0,0 +1,10 @@
+﻿//User Input
+Console.WriteLine("-----User Input-----");
+String? name;
+Console.Write("Enter Name: ");
+name = Console.ReadLine();
+Console.Write("Enter Age: ");
+int age = Convert.ToInt32(Console.ReadLine());
+//Instead of: int ageInt = Convert.ToInt32(age);
+Console.WriteLine("Name: " + name);
+Console.WriteLine("Age: " + age);
