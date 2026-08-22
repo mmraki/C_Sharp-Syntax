c9840a182b68a4a5598da29b927663aa372f5b8e
Meraki <convallariameraki@gmail.com>
2026-08-22 17:41:29 +0800

    Type Casting

---
+++ Chapter_03/Type_Casting/Program.cs
@@ -0,0 +1,19 @@
+﻿//Type Casting
+Console.WriteLine("-----Type Casting/Conversion-----");
+double a = 1;
+int b = Convert.ToInt32(a);
+int c = 100;
+double d = Convert.ToDouble(c);
+string e = "%";//In order to convert a string to a char using Convert.ToChar it has to be a string with only one character
+char f = Convert.ToChar(e);
+
+Console.WriteLine("Double: " + a);
+Console.WriteLine("Double to Integer: " + b);
+Console.WriteLine("Integer to Double: " + d);
+Console.WriteLine("Type: " + a.GetType());
+Console.WriteLine("Char: " + f);
+
+String situation = "True";
+bool trueOrFalse = Convert.ToBoolean(situation);
+
+Console.WriteLine("State: " + trueOrFalse);
