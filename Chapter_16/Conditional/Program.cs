373195e11e8ca9a88480350808b69ac3c86bba48
Meraki <convallariameraki@gmail.com>
2026-08-22 17:47:30 +0800

    Conditional Operators

---
+++ Chapter_16/Conditional/Program.cs
@@ -0,0 +1,16 @@
+﻿//Conditional Operator = Shortened if else
+double temp;
+String message;
+Console.WriteLine("Enter Temperature: ");
+temp = Convert.ToDouble(Console.ReadLine());
+/*
+if (temp <= 50){
+    message = "Its Cold";
+}else{
+    message = "Its Hot";
+}
+*/
+message = (temp <= 50) ? "Its Fuckin Cold": "Its Fuckin Hot";
+Console.WriteLine(message);
+
+Console.WriteLine((temp <= 50) ? "Its Fuckin Cold" : "Its Fuckin Hot");
