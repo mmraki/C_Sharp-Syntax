f70c155862059e5f04dd6793c7aead1bd9c474f7
Meraki <convallariameraki@gmail.com>
2026-08-22 17:55:01 +0800

    Polymorphism

---
+++ Chapter_25/Polymorphism/Program.cs
@@ -0,0 +1,41 @@
+﻿//Polymorphism = Having many forms
+//similarities
+
+Human human = new Human();
+Dog dog = new Dog();
+Cheetah cheetah = new Cheetah();
+
+Creature[] creatures = { human, dog, cheetah };
+
+foreach (Creature racer in creatures)
+{
+    racer.Go();
+}
+class Creature
+{
+    public virtual void Go()
+    {
+
+    }
+}
+class Human : Creature
+{
+    public override void Go()
+    {
+        Console.WriteLine("Fast");
+    }
+}
+class Dog : Creature
+{
+    public override void Go()
+    {
+        Console.WriteLine("Faster");
+    }
+}
+class Cheetah : Creature
+{
+    public override void Go()
+    {
+        Console.WriteLine("Fastest");
+    }
+}
